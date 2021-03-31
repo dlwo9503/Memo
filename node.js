9d6c4132370ef893879fd39365f951b447c265a0
Lee Jae-Seong <74041698+dlwo9503@users.noreply.github.com>
2021-03-31 23:32:23 +0900

    Create node.js

---
+++ node.js
@@ -0,0 +1,13 @@
+npm : 노드 패키지 매니지먼트, 오픈소스 관리
+
+설치하는 모듈들은 package-lock.json 안에 dependencies 부분에 설치됨
+
+nodemailer
+ - 메일 보내는 오픈 소스
+ - npm install nodemailer // 노드메일러 설치
+ - https://nodemailer.com // 노드메일러 사용법
+ - https://mailtrap.io / 메일서버 대행 서비스
+ - const nodemailer = require('nodemailer'); // 노드메일러 불러오기
+
+mysql
+ - https://www.npmjs.com/package/mysql // 사이트
